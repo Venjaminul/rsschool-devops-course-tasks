04ae59a3b31d6c89af8f1bee06adfa07213d8b32
Venjaminul <melnicdorin@yahoo.com>
2025-06-12 21:01:16 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
-provder "aws" {
+provider "aws" {
   region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "course-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
 }
